f547553d6855e0ba8bad53a29faa265ee406f76f
Jérémie Noël <dev.jnoel@gmail.com>
2020-01-28 15:59:58 +0100

    fix(login): adapt login for formik form

--- client/src/pages/User/Login.vue
+++ client/src/pages/User/Login.vue
@@ -11,16 +11,21 @@
     <h1 class="text-center login-title">Login to your account</h1>
   <form class="bg-white text-center  shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="login" name="login">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="login">
         Login
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+      <Field class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+      type="email" name="login" placeholder="Email" tag="input" />
     </div>
     <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password" name="password">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="*****">
+      <Field class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      type="password"
+            name="password"
+            placeholder="Password"
+            tag="input" />
     </div>
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
