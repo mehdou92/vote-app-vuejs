187c385d387acff4c08677be0d810fb4dc3bd851
benjamin <kazmierczakbenjamin@gmail.com>
2020-01-28 20:06:53 +0100

    correction liens btn go home

--- client/src/pages/error/404.vue
+++ client/src/pages/error/404.vue
@@ -5,7 +5,9 @@
           <div class="text-black text-5xl md:text-15xl font-black">404</div>
           <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
           <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">Sorry, the page you are looking for could not be found.</p>
-          <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">Go Home</button>
+          <router-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">Go Home</button>
+          </router-link>
         </div>
       </div>
       <div class="relative pb-full md:flex md:pb-0 md:min-h-screen w-full md:w-1/2">
